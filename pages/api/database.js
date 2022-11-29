284baeaaeb40e1865a5a5c82c67e31dd5e834939
unknown <rapha-sm2015@hotmail.com>
2022-11-29 16:27:47 -0300

    mongodb server connect

--- pages/api/database.js
+++ pages/api/database.js
@@ -1,6 +1,11 @@
-export default function handler(req, res) {
-  res.status(200).json({ 
-    username: process.env.DBUSERNAME, 
-    password: process.env.DBPASSWORD
-   })
+const { MongoClient } = require('mongodb')
+const uri = process.env.MONGODB_URI
+const client = new MongoClient(uri)
+
+const database = async (req, res) => {
+  if(method === "GET") (
+    res.status(200).json({method: "GET"})
+  )
 }
+
+export default database
