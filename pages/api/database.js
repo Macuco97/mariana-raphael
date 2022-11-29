288912988e2efb89943e92d87af4a0891d04d348
unknown <rapha-sm2015@hotmail.com>
2022-11-29 16:36:47 -0300

    Create method filter

--- pages/api/database.js
+++ pages/api/database.js
@@ -1,11 +1,25 @@
+import { RESPONSE_LIMIT_DEFAULT } from 'next/dist/server/api-utils'
+
 const { MongoClient } = require('mongodb')
 const uri = process.env.MONGODB_URI
 const client = new MongoClient(uri)
 
 const database = async (req, res) => {
-  if(method === "GET") (
-    res.status(200).json({method: "GET"})
-  )
+  const { method } = req
+
+  switch(method) {
+    case "GET":
+      res.status(200).json({method: "GET"})
+      break;
+    case "POST": 
+      res.status(200).json({method: "POST"})
+      break;
+    case "PUT":
+      res.status(200).json({method: "PUT"})
+      break;
+    case "DELETE":
+      res.status(200).json({method: "DELETE"})
+  }
 }
 
 export default database
