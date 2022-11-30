d5bb68fd4e1f4748f65a663bf6bf486500c471d1
unknown <rapha-sm2015@hotmail.com>
2022-11-30 12:24:31 -0300

    Upload throught multer

--- pages/api/database.js
+++ pages/api/database.js
@@ -1,25 +1,44 @@
-import { RESPONSE_LIMIT_DEFAULT } from 'next/dist/server/api-utils'
+import nc from "next-connect"
+import multer from 'multer'
 
-const { MongoClient } = require('mongodb')
-const uri = process.env.MONGODB_URI
-const client = new MongoClient(uri)
+const uploadPath = __dirname
+console.log(uploadPath)
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: './public/uploads',
+    filename: (req, file, cb) => cb(null, file.originalname),
+  }),
+});
 
-const database = async (req, res) => {
-  const { method } = req
 
-  switch(method) {
-    case "GET":
-      res.status(200).json({method: "GET"})
-      break;
-    case "POST": 
-      res.status(200).json({method: "POST"})
-      break;
-    case "PUT":
-      res.status(200).json({method: "PUT"})
-      break;
-    case "DELETE":
-      res.status(200).json({method: "DELETE"})
-  }
-}
+const database = nc({
+  onError: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).end(err.stack);
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end("Page is not found");
+  },
+})
+  .use(upload.single('image'))
+  .get((req, res) => {
+    const file  = req.file
+    res.send('Upload sucess');
+  })
+  .post((req, res) => {
+    res.json({ hello: "world" });
+  })
+  .put(async (req, res) => {
+    res.end("async/await is also supported!");
+  })
+  .patch(async (req, res) => {
+    throw new Error("Throws me around! Error can be caught and handled.");
+  });
 
 export default database
+
+export const config = {
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+}
